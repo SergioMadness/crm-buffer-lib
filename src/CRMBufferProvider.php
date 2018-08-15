1f09225e774f783062e2f7cf6b0a3b95d875a534
SergioMadness <madness.sergio@gmail.com>
2018-08-15 17:05:24 +0300

    Register Payment model

--- src/CRMBufferProvider.php
+++ src/CRMBufferProvider.php
@@ -2,11 +2,13 @@
 
 use Illuminate\Support\ServiceProvider;
 use professionalweb\crmbuffer\Models\Lead;
+use professionalweb\crmbuffer\Models\Payment;
 use professionalweb\crmbuffer\Models\Request;
 use professionalweb\crmbuffer\Models\Contact;
 use professionalweb\crmbuffer\Facades\CRMBuffer;
 use professionalweb\crmbuffer\Interfaces\Lead as ILead;
 use professionalweb\crmbuffer\Services\CRMBufferService;
+use professionalweb\crmbuffer\Interfaces\Payment as IPayment;
 use professionalweb\crmbuffer\Interfaces\Request as IRequest;
 use professionalweb\crmbuffer\Interfaces\Contact as IContact;
 use professionalweb\crmbuffer\Interfaces\Transport as ITransport;
@@ -19,6 +21,7 @@ class CRMBufferProvider extends ServiceProvider
         $this->app->bind(IRequest::class, Request::class);
         $this->app->bind(ILead::class, Lead::class);
         $this->app->bind(IContact::class, Contact::class);
+        $this->app->bind(IPayment::class, Payment::class);
         $this->app->bind(ITransport::class, function () {
             return new Transport(
                 config('crm-buffer.url'),
